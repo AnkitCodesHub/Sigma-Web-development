56b4e48b34d713f2ddcfb875a00ff4e6a567b207
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-02-16 15:21:20 +0530

    commit

--- Design of layout/main.js
+++ Design of layout/main.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.set('view engine','ejs');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index',{foo:'FOO'});
 })
 
 app.listen(port, () => {
