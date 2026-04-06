54db58e66ab9a0cd3647c6fb6f3b1806d62c5374
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-06 11:51:05 +0530

    commit

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 3000
 app.set('view engine','ejs');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index', {foo: 'FOO'});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
